9864c4952cac7de60dc640666d9d6bce6954582f
Ryan Raba <ryanraba@gmail.com>
2021-11-05 08:47:34 -0700

    copy option descriptions from casadocs api

--- casaconfig/config.py
+++ casaconfig/config.py
@@ -1,20 +1,65 @@
-import os
-import time
-import sys
-import pkg_resources
+import os, time, sys, pkg_resources
 from casaconfig import measures_update, pull_data
 
 
-########################################################################
-## Setup the runtime operation data directories
-########################################################################
+# list of paths where CASA should search for data subdirectories
 datapath = [pkg_resources.resource_filename('casaconfig', '__data__/')]
+
+# location of required runtime measures data, takes precedence over location(s) in datapath list
 rundata = os.path.expanduser("~/.casa/measures")
 
-########################################################################
-## Define the logfile naming scheme
-########################################################################
+# log file path/name
 logfile='casalog_%s.log' % time.strftime("%Y-%m-%d", time.localtime())
+
+# do not create a log file when True, If True, then any logfile value is ignored and there is no log file
+nologfile = False
+
+# print log output to terminal when True (in addition to any logfile and CASA logger)
+log2term = False
+
+# do not start the CASA logger when True
+nologger = False
+
+# avoid starting GUI tools when True. If True then the CASA logger is not started even if nologger is False
+nogui = False
+
+# the IPython prompt color scheme. Must be one of “Neutral”, “NoColor”, “Linux” or “LightBG”, default “Neutral”
+# if an invalid color is given a warning message is printed and logged but CASA continues using the default color
+colors = "LightBG"
+
+# startup without a graphical backend if True
+agg = False
+
+# attempt to load the pipeline modules and set other options appropriate for pipeline use if True
+# when pipeline is True then agg will be assumed to be true even if agg is set to False here or on the command line
+pipeline = False
+
+# create and use an IPython log in the current directory if True
+iplog = True
+
+# allow anonymous usage reporting
+telemetry_enabled = True
+
+# location to place telemetry data prior to reporting
+telemetry_log_directory = os.path.expanduser("~/.casa/telemetry")
+
+# maximum size of telemetry recording
+telemetry_log_limit = 1650
+
+# telemetry recording size that triggers a report
+telemetry_log_size_interval = 30
+
+# telemetry recording report frequency
+telemetry_submit_interval = 20
+
+# allow anonymous crash reporting
+crashreporter_enabled = True
+
+# include the user’s local site-packages in the python path if True. 
+# normally these are excluded to avoid any conflicts with CASA modules
+user_site = False
+
+
 
 ########################################################################
 ## Define what happens when CASA starts
@@ -50,22 +95,4 @@ if __name__.startswith('casatool'):
     measures_update(rundata, logger=logger)
 
 
-########################################################################
-## Rest of the default CASA configuration parameters
-########################################################################
-telemetry_enabled = True
-crashreporter_enabled = True
-nologfile = False
-log2term = True
-nologger = True
-nogui = False
-colors = "LightBG"
-agg = False
-pipeline = False
-iplog = True
-user_site = False
-telemetry_log_directory = os.path.expanduser("~/.casa/telemetry")
-telemetry_log_limit = 1650
-telemetry_log_size_interval = 30
-telemetry_submit_interval = 20
 
