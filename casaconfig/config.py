a1cb42544e98cb65c799d1d7732005bf14565bef
Bob Garwood <bgarwood@nrao.edu>
2022-05-09 11:54:05 -0400

    casalog default back to current default : casa-%s.log with the time string built from gmtime not localtime

--- casaconfig/config.py
+++ casaconfig/config.py
@@ -15,7 +15,7 @@ populate_data = True
 measures_update = True
 
 # log file path/name
-logfile='casalog_%s.log' % time.strftime("%Y%m%d-%H%M%S", time.localtime())
+logfile='casa-%s.log' % time.strftime("%Y%m%d-%H%M%S", time.gmtime())
 
 # do not create a log file when True, If True, then any logfile value is ignored and there is no log file
 nologfile = False
