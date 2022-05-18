0137be52a4f4c021d32b691cdf4ecb0e495959a5
Darrell Schiebel <darrell@schiebel.us>
2022-05-18 11:51:37 -0400

    add history func + --noconfig & --configpath [CAS-13116]

--- casaconfig/config.py
+++ casaconfig/config.py
@@ -31,26 +31,44 @@ DO NOT ADD new configuration variables here. Instead, add them in
 _config_defaults.py (found in the same directory as this file).
 '''
 from . import _config_defaults
+import argparse as __argparse
+import sys as __sys
+import os as __os
 
 ## list of config variables
 __defaults = [ x for x in dir(_config_defaults) if not x.startswith('_') ]
 
-## files to be evaluated/loaded
-__config_files = [ 'casaconfigsite', '~/.casa/config.py' ]
+## look for arguments that affect casaconfig
+__parser = __argparse.ArgumentParser( )
+__parser.add_argument( "--noconfig", dest='noconfig', action='store_const', const=True, default=False,
+                       help='do not load user config.py' )
+__parser.add_argument( "--configpath",dest='configpath', default='~/.casa',
+                            help='location for startup files, internal working files')
+__flags,__args = __parser.parse_known_args(__sys.argv)
+__user_config = [ ] if __flags.noconfig else [ __os.path.join(__flags.configpath,'config.py') ]
 
-import pkgutil as _pkgutil
-import os as _os
+## files to be evaluated/loaded
+__config_files = [ 'casaconfigsite', *__user_config ]
+__loaded_config_files = [ __file__ ]
+
+import pkgutil as __pkgutil
 
 ## evaluate config files
-for __f in [ _os.path.expanduser(f) for f in __config_files ]:
-    if __f.find('/') >= 0 and _os.path.exists(__f):
+for __f in [ __os.path.expanduser(f) for f in __config_files ]:
+    if __f.find('/') >= 0 and __os.path.exists(__f):
         ## config file is a fully qualified path
         exec(open(__f).read( ),_config_defaults._globals( ))
+        __loaded_config_files.append( __f )
     else:
         ## config file is a package name
-        __pkg = _pkgutil.get_loader(__f)
+        __pkg = __pkgutil.get_loader(__f)
         if __pkg is not None:
             exec(open(__pkg.get_filename( )).read( ),_config_defaults._globals( ))
+            __loaded_config_files.append( __pkg.get_filename( ) )
+
 
 for __v in __defaults:
     globals()[__v] = getattr(_config_defaults,__v,None)
+
+def load_history( ):
+    return __loaded_config_files
