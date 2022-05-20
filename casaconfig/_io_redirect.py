4b68aecbcb5f74c824c20aa7e48770816d4853e1
Darrell Schiebel <darrell@schiebel.us>
2022-05-20 11:59:21 -0400

    throw away config file output with module execution (-m flag) [CAS-13116]

---
+++ casaconfig/_io_redirect.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from contextlib import contextmanager
+
+def _fileno(file_or_fd):
+    fd = getattr(file_or_fd, 'fileno', lambda: file_or_fd)()
+    if not isinstance(fd, int):
+        raise ValueError("Expected a file (`.fileno()`) or a file descriptor")
+    return fd
+
+@contextmanager
+def stdout_redirected(to=os.devnull, stdout=None):
+    if stdout is None:
+       stdout = sys.stdout
+
+    stdout_fd = _fileno(stdout)
+    # copy stdout_fd before it is overwritten
+    #NOTE: `copied` is inheritable on Windows when duplicating a standard stream
+    with os.fdopen(os.dup(stdout_fd), 'wb') as copied:
+        stdout.flush()  # flush library buffers that dup2 knows nothing about
+        try:
+            os.dup2(_fileno(to), stdout_fd)  # $ exec >&to
+        except ValueError:  # filename
+            with open(to, 'wb') as to_file:
+                os.dup2(to_file.fileno(), stdout_fd)  # $ exec > to
+        try:
+            yield stdout # allow code to be run with the redirected stdout
+        finally:
+            # restore stdout to its previous value
+            #NOTE: dup2 makes stdout_fd inheritable unconditionally
+            stdout.flush()
+            os.dup2(copied.fileno(), stdout_fd)  # $ exec >&copied
+
+@contextmanager
+def redirect_stdout(new_target):
+    old_target, sys.stdout = sys.stdout, new_target # replace sys.stdout
+    try:
+        yield new_target # run some code with the replaced stdout
+    finally:
+        sys.stdout = old_target # restore to the previous value
+
+def merged_stderr_stdout():  # $ exec 2>&1
+    return stdout_redirected(to=sys.stdout, stdout=sys.stderr)
+
+@contextmanager
+def no_redirect( ):
+    yield None
--- casaconfig/config.py
+++ casaconfig/config.py
@@ -35,6 +35,8 @@ import argparse as __argparse
 import traceback as __traceback
 import sys as __sys
 import os as __os
+import pkgutil as __pkgutil
+from . import _io_redirect as _io
 
 ## list of config variables
 __defaults = [ x for x in dir(_config_defaults) if not x.startswith('_') ]
@@ -53,34 +55,41 @@ __config_files = [ 'casaconfigsite', *__user_config ]
 __loaded_config_files = [ __file__ ]
 __errors_encountered = { }
 
-import pkgutil as __pkgutil
 
 ## evaluate config files
-for __f in [ __os.path.expanduser(f) for f in __config_files ]:
-    if __f.find('/') >= 0 and __os.path.exists(__f):
-        ## config file is a fully qualified path
-        try:
-            __orig = { k: _config_defaults._globals( )[k] for k in __defaults }
-            exec( open(__f).read( ), __orig )
-        except Exception as e:
-            __errors_encountered[__f] = __traceback.format_exc( )
-        else:
-            for __v in __defaults:
-                _config_defaults._globals( )[__v] = __orig[__v]
-            __loaded_config_files.append( __f )
-    else:
-        ## config file is a package name
-        __pkg = __pkgutil.get_loader(__f)
-        if __pkg is not None:
+## ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+## If the the execution mode of python is module execution (e.g. "python3 -m casatools") then
+## the output from the configuration files is thrown away. Otherwise, all output from the
+## configuration files is passed through. This allows for output from module execution to
+## be used by scripts.
+##
+_module_execution = len(__sys.argv) > 0 and __sys.argv[0] == '-m'
+with (_io.stdout_redirected(to=__os.devnull), _io.merged_stderr_stdout( )) if _module_execution else _io.no_redirect( ):
+    for __f in [ __os.path.expanduser(f) for f in __config_files ]:
+        if __f.find('/') >= 0 and __os.path.exists(__f):
+            ## config file is a fully qualified path
             try:
                 __orig = { k: _config_defaults._globals( )[k] for k in __defaults }
-                exec(open(__pkg.get_filename( )).read( ),__orig)
+                exec( open(__f).read( ), __orig )
             except Exception as e:
-                __errors_encountered[__pkg.get_filename( )] = __traceback.format_exc( )
+                __errors_encountered[__f] = __traceback.format_exc( )
             else:
                 for __v in __defaults:
                     _config_defaults._globals( )[__v] = __orig[__v]
-                __loaded_config_files.append( __pkg.get_filename( ) )
+                __loaded_config_files.append( __f )
+        else:
+            ## config file is a package name
+            __pkg = __pkgutil.get_loader(__f)
+            if __pkg is not None:
+                try:
+                    __orig = { k: _config_defaults._globals( )[k] for k in __defaults }
+                    exec(open(__pkg.get_filename( )).read( ),__orig)
+                except Exception as e:
+                    __errors_encountered[__pkg.get_filename( )] = __traceback.format_exc( )
+                else:
+                    for __v in __defaults:
+                        _config_defaults._globals( )[__v] = __orig[__v]
+                    __loaded_config_files.append( __pkg.get_filename( ) )
 
 
 for __v in __defaults:
