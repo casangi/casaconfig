742a551b4a85a904cdefa069b35b55136e5160d4
Darrell Schiebel <darrell@schiebel.us>
2022-08-01 21:59:27 -0400

    replace write_default_config( ) with getconfig( )

--- casaconfig/__init__.py
+++ casaconfig/__init__.py
@@ -7,5 +7,13 @@ from .measures_update import measures_update
 from .measures_available import measures_available
 from .set_casacore_path import set_casacore_path
 from .pull_data import pull_data
-from .write_default_config import write_default_config
 
+def getconfig( ):
+    '''Get configuration values as strings which can be logged, stored or evaluated.
+
+    Returns
+    -------
+    list[str]            list of configuration strings
+    '''
+    from . import _config_defaults as cdf
+    return list( map( lambda name: f'{name} = {repr(getattr(cdf,name))}', filter( lambda x: not x.startswith('_'), dir(cdf) ) ) )
